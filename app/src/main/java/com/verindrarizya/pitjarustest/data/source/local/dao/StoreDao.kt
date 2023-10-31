16423ce9ecb46dbc3cd6dcf635f6a530d0c2de54
verindrarizya <v.rizya22@gmail.com>
2023-10-31 21:16:47 +0700

    MainActivity ui

--- app/src/main/java/com/verindrarizya/pitjarustest/data/source/local/dao/StoreDao.kt
+++ app/src/main/java/com/verindrarizya/pitjarustest/data/source/local/dao/StoreDao.kt
@@ -3,6 +3,7 @@ package com.verindrarizya.pitjarustest.data.source.local.dao
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.verindrarizya.pitjarustest.data.source.local.entity.StoreEntity
 
@@ -12,7 +13,7 @@ interface StoreDao {
     @Query("SELECT * FROM store")
     fun getAllStore(): LiveData<StoreEntity>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAllStore(storeEntities: List<StoreEntity>)
 
 }
--- app/src/main/java/com/verindrarizya/pitjarustest/presentation/main/MainActivity.kt
+++ app/src/main/java/com/verindrarizya/pitjarustest/presentation/main/MainActivity.kt
@@ -2,13 +2,18 @@ package com.verindrarizya.pitjarustest.presentation.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.verindrarizya.pitjarustest.R
+import com.verindrarizya.pitjarustest.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
